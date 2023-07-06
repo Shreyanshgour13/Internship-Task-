ceadcb6c48082a1e3171dfd861639a6761bb3ab7
Shreyanshgour13 <88890545+Shreyanshgour13@users.noreply.github.com>
2023-07-06 14:21:19 +0530

    comming all changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Box, Checkbox, CircularProgress, Container, Typography } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.gyanibooks.com/library/get_dummy_notes'
+        );
+        setNotes(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching notes:', error);
+      }
+    };
+
+    fetchNotes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+    <Container maxWidth="sm">
+      <Typography variant="h3" align="center" gutterBottom>
+        Dummy Notes
+      </Typography>
+      {loading ? (
+        <Box display="flex" justifyContent="center" mt={5}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <ul>
+          {notes.map((note) => (
+            <li key={note.id}>
+              <Typography variant="h5" mt={2} >{note.title}</Typography>
+              
+            </li>
+          ))}
+        </ul>
+      )}
+    </Container>
+  </ThemeProvider>
   );
 }
 
 export default App;
+
+
+
